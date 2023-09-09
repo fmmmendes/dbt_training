af7fa0d60b8e65ecdb56dca75027fb75644da48d
Filipe Mendes <filipe.mendes@radicant.com>
2023-09-09 16:21:30 +0100

    format jinja

--- dbt_training/models/count_orders.sql
+++ dbt_training/models/count_orders.sql
@@ -1,7 +1,7 @@
 select  c.id as customer_id, 
 concat(c.first_name, ' ' ,c.last_name) full_name, 
 count(o.id) as count_orders
-from northwind.customers as c
-join northwind.orders as o on c.id = o.customer_id 
+from {{ source('northwind', 'customers') }} as c
+join {{ source('northwind', 'orders') }} as o on c.id = o.customer_id 
 group by c.id, full_name
 order by count_orders desc
