d3e0ad4ec7878f9c4e092fb7d1235839cc00c83f
Filipe Mendes <filipe.mendes@radicant.com>
2023-09-09 15:57:18 +0100

    update run, add clean

---
+++ dbt-clean.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+dbt clean --project-dir=dbt_training --profiles-dir=dbt_profiles --profile=dbt_training
--- dbt-run.sh
+++ dbt-run.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
 
 dbt run --project-dir=dbt_training --profiles-dir=dbt_profiles --profile=dbt_training
+
+dbt docs generate --project-dir=dbt_training --profiles-dir=dbt_profiles --profile=dbt_training
