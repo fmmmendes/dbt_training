be978170704c170e90ce658b724677ea87e04d9c
Filipe Mendes <filipe.mendes@radicant.com>
2023-09-09 10:45:42 +0100

    update northwind-psql sql and readme

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
+# DBT Training
+
+
+## Setup with Docker Compose
+
+Create a local database with Docker Compose. The database is a Postgres database with the Northwind database loaded.
 
 
 ```bash
@@ -9,6 +15,46 @@ docker-compose -f docker-compose-dbt-db.yml up
 
 # remove
 docker-compose -f docker-compose-dbt-db.yml down
+```
 
+
+## Use Case - Northwind
+
+This use cases Northwind database. The database is a sample database that was originally created by Microsoft and used as the basis for their tutorials in a variety of database products for decades. The database itself is no longer maintained by Microsoft, but you can still download the data and use it to learn about SQL and database concepts.
+
+https://github.com/dalers/mywind/blob/master/README.md
+
+List of tables
+
+- northwind.employees
+- northwind.customers
+- northwind.sales_reports
+- northwind.strings
+- northwind.employee_privileges
+- northwind.privileges
+- northwind.inventory_transaction_types
+- northwind.orders
+- northwind.shippers
+- northwind.orders_tax_status
+- northwind.orders_status
+- northwind.products
+- northwind.purchase_orders
+- northwind.purchase_order_status
+- northwind.suppliers
+- northwind.inventory_transactions
+- northwind.invoices
+- northwind.order_details
+- northwind.order_details_status
+- northwind.purchase_order_details
+
+List all northwind tables in a sql query
+
+```sql
+select * from pg_catalog.pg_tables pt
+where schemaname = 'northwind';
+```
+
+
+## References
 
 https://github.com/dalers/mywind/blob/master/README.md
--- sql/northwind-psql.sql
+++ sql/northwind-psql.sql
@@ -1,4 +1,3 @@
-
 DROP SCHEMA IF EXISTS northwind ;
 CREATE SCHEMA IF NOT EXISTS northwind ;
 
@@ -53,12 +52,12 @@ CREATE TABLE IF NOT EXISTS northwind.employees (
   attachments BYTEA
 );
 
-CREATE INDEX idx_city ON northwind.employees (city);
-CREATE INDEX idx_company ON northwind.employees (company);
-CREATE INDEX idx_first_name ON northwind.employees (first_name);
-CREATE INDEX idx_last_name ON northwind.employees (last_name);
-CREATE INDEX idx_zip_postal_code ON northwind.employees (zip_postal_code);
-CREATE INDEX idx_state_province ON northwind.employees (state_province);
+CREATE INDEX idx_city_c ON northwind.employees (city);
+CREATE INDEX idx_company_c ON northwind.employees (company);
+CREATE INDEX idx_first_name_c ON northwind.employees (first_name);
+CREATE INDEX idx_last_name_c ON northwind.employees (last_name);
+CREATE INDEX idx_zip_postal_code_c ON northwind.employees (zip_postal_code);
+CREATE INDEX idx_state_province_c ON northwind.employees (state_province);
 
 
 
@@ -118,12 +117,12 @@ CREATE TABLE IF NOT EXISTS northwind.shippers (
   attachments BYTEA
 );
 
-CREATE INDEX idx_city ON northwind.shippers (city);
-CREATE INDEX idx_company ON northwind.shippers (company);
-CREATE INDEX idx_first_name ON northwind.shippers (first_name);
-CREATE INDEX idx_last_name ON northwind.shippers (last_name);
-CREATE INDEX idx_zip_postal_code ON northwind.shippers (zip_postal_code);
-CREATE INDEX idx_state_province ON northwind.shippers (state_province);
+CREATE INDEX idx_city_sh ON northwind.shippers (city);
+CREATE INDEX idx_company_sh ON northwind.shippers (company);
+CREATE INDEX idx_first_name_sh ON northwind.shippers (first_name);
+CREATE INDEX idx_last_name_sh ON northwind.shippers (last_name);
+CREATE INDEX idx_zip_postal_code_sh ON northwind.shippers (zip_postal_code);
+CREATE INDEX idx_state_province_sh ON northwind.shippers (state_province);
 
 
 CREATE TABLE IF NOT EXISTS northwind.orders_tax_status (
@@ -139,4 +138,280 @@ CREATE TABLE IF NOT EXISTS northwind.orders_status (
   PRIMARY KEY (id)
 );
 
+
+CREATE TABLE IF NOT EXISTS northwind.orders (
+  id SERIAL PRIMARY KEY,
+  employee_id INT,
+  customer_id INT,
+  order_date TIMESTAMP,
+  shipped_date TIMESTAMP,
+  shipper_id INT,
+  ship_name VARCHAR(50),
+  ship_address TEXT,
+  ship_city VARCHAR(50),
+  ship_state_province VARCHAR(50),
+  ship_zip_postal_code VARCHAR(50),
+  ship_country_region VARCHAR(50),
+  shipping_fee NUMERIC(19, 4) DEFAULT '0.0000',
+  taxes NUMERIC(19, 4) DEFAULT '0.0000',
+  payment_type VARCHAR(50),
+  paid_date TIMESTAMP,
+  notes TEXT,
+  tax_rate DOUBLE PRECISION DEFAULT '0',
+  tax_status_id SMALLINT,
+  status_id SMALLINT DEFAULT '0',
+  FOREIGN KEY (customer_id) REFERENCES northwind.customers(id) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  FOREIGN KEY (employee_id) REFERENCES northwind.employees(id) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  FOREIGN KEY (shipper_id) REFERENCES northwind.shippers(id) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  FOREIGN KEY (tax_status_id) REFERENCES northwind.orders_tax_status(id) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  FOREIGN KEY (status_id) REFERENCES northwind.orders_status(id) ON DELETE NO ACTION ON UPDATE NO ACTION
+);
+
+
+CREATE TABLE IF NOT EXISTS northwind.products (
+  supplier_ids TEXT,
+  id SERIAL PRIMARY KEY,
+  product_code VARCHAR(25),
+  product_name VARCHAR(50),
+  description TEXT,
+  standard_cost NUMERIC(19,4) DEFAULT '0.0000',
+  list_price NUMERIC(19,4) NOT NULL DEFAULT '0.0000',
+  reorder_level INT,
+  target_level INT,
+  quantity_per_unit VARCHAR(50),
+  discontinued BOOLEAN NOT NULL DEFAULT false,
+  minimum_reorder_quantity INT,
+  category VARCHAR(50),
+  attachments BYTEA
+);
+
+
+CREATE TABLE IF NOT EXISTS northwind.purchase_order_status (
+  id INT NOT NULL,
+  status VARCHAR(50) DEFAULT NULL,
+  PRIMARY KEY (id)
+);
+
+
+CREATE TABLE IF NOT EXISTS northwind.suppliers (
+  id SERIAL PRIMARY KEY,
+  company VARCHAR(50),
+  last_name VARCHAR(50),
+  first_name VARCHAR(50),
+  email_address VARCHAR(50),
+  job_title VARCHAR(50),
+  business_phone VARCHAR(25),
+  home_phone VARCHAR(25),
+  mobile_phone VARCHAR(25),
+  fax_number VARCHAR(25),
+  address TEXT,
+  city VARCHAR(50),
+  state_province VARCHAR(50),
+  zip_postal_code VARCHAR(15),
+  country_region VARCHAR(50),
+  web_page TEXT,
+  notes TEXT,
+  attachments BYTEA
+);
+
+-- Create indexes
+CREATE INDEX IF NOT EXISTS idx_city_sp ON northwind.suppliers (city);
+CREATE INDEX IF NOT EXISTS idx_company_sp ON northwind.suppliers (company);
+CREATE INDEX IF NOT EXISTS idx_first_name_sp ON northwind.suppliers (first_name);
+CREATE INDEX IF NOT EXISTS idx_last_name_sp ON northwind.suppliers (last_name);
+CREATE INDEX IF NOT EXISTS idx_zip_postal_code_sp ON northwind.suppliers (zip_postal_code);
+CREATE INDEX IF NOT EXISTS idx_state_province_sp ON northwind.suppliers (state_province);
+
+
+CREATE TABLE IF NOT EXISTS northwind.purchase_orders (
+  id SERIAL PRIMARY KEY,
+  supplier_id INT,
+  created_by INT,
+  submitted_date TIMESTAMP,
+  creation_date TIMESTAMP,
+  status_id INT DEFAULT '0',
+  expected_date TIMESTAMP,
+  shipping_fee NUMERIC(19, 4) NOT NULL DEFAULT '0.0000',
+  taxes NUMERIC(19, 4) NOT NULL DEFAULT '0.0000',
+  payment_date TIMESTAMP,
+  payment_amount NUMERIC(19, 4) DEFAULT '0.0000',
+  payment_method VARCHAR(50),
+  notes TEXT,
+  approved_by INT,
+  approved_date TIMESTAMP,
+  submitted_by INT,
+  CONSTRAINT fk_purchase_orders_employees1
+    FOREIGN KEY (created_by)
+    REFERENCES northwind.employees (id)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT fk_purchase_orders_purchase_order_status1
+    FOREIGN KEY (status_id)
+    REFERENCES northwind.purchase_order_status (id)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT fk_purchase_orders_suppliers1
+    FOREIGN KEY (supplier_id)
+    REFERENCES northwind.suppliers (id)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION
+);
+
+-- Create indexes
+CREATE UNIQUE INDEX IF NOT EXISTS idx_id_po ON northwind.purchase_orders (id);
+CREATE INDEX IF NOT EXISTS idx_created_by_po ON northwind.purchase_orders (created_by);
+CREATE INDEX IF NOT EXISTS idx_status_id_po ON northwind.purchase_orders (status_id);
+CREATE INDEX IF NOT EXISTS idx_supplier_id_po ON northwind.purchase_orders (supplier_id);
+
+
+CREATE TABLE IF NOT EXISTS northwind.inventory_transactions (
+  id SERIAL PRIMARY KEY,
+  transaction_type SMALLINT NOT NULL,
+  transaction_created_date TIMESTAMP,
+  transaction_modified_date TIMESTAMP,
+  product_id INT NOT NULL,
+  quantity INT NOT NULL,
+  purchase_order_id INT,
+  customer_order_id INT,
+  comments VARCHAR(255),
+  CONSTRAINT fk_inventory_transactions_orders1
+    FOREIGN KEY (customer_order_id)
+    REFERENCES northwind.orders (id)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT fk_inventory_transactions_products1
+    FOREIGN KEY (product_id)
+    REFERENCES northwind.products (id)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT fk_inventory_transactions_purchase_orders1
+    FOREIGN KEY (purchase_order_id)
+    REFERENCES northwind.purchase_orders (id)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT fk_inventory_transactions_inventory_transaction_types1
+    FOREIGN KEY (transaction_type)
+    REFERENCES northwind.inventory_transaction_types (id)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION
+);
+
+-- Create indexes
+CREATE INDEX IF NOT EXISTS idx_customer_order_id ON northwind.inventory_transactions (customer_order_id);
+CREATE INDEX IF NOT EXISTS idx_product_id ON northwind.inventory_transactions (product_id);
+CREATE INDEX IF NOT EXISTS idx_purchase_order_id ON northwind.inventory_transactions (purchase_order_id);
+CREATE INDEX IF NOT EXISTS idx_transaction_type ON northwind.inventory_transactions (transaction_type);
+
+
+CREATE TABLE IF NOT EXISTS northwind.invoices (
+  id SERIAL PRIMARY KEY,
+  order_id INT,
+  invoice_date TIMESTAMP,
+  due_date TIMESTAMP,
+  tax NUMERIC(19, 4) DEFAULT '0.0000',
+  shipping NUMERIC(19, 4) DEFAULT '0.0000',
+  amount_due NUMERIC(19, 4) DEFAULT '0.0000',
+  CONSTRAINT fk_invoices_orders1
+    FOREIGN KEY (order_id)
+    REFERENCES northwind.orders (id)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION
+);
+
+-- Create indexes
+CREATE INDEX IF NOT EXISTS idx_id ON northwind.invoices (id);
+CREATE INDEX IF NOT EXISTS idx_fk_invoices_orders1 ON northwind.invoices (order_id);
+
+--------
+CREATE TABLE IF NOT EXISTS northwind.order_details_status (
+  id INT NOT NULL,
+  status_name VARCHAR(50) NOT NULL,
+  PRIMARY KEY (id)
+);
+
+
+CREATE TABLE IF NOT EXISTS northwind.order_details (
+  id SERIAL PRIMARY KEY,
+  order_id INT NOT NULL,
+  product_id INT,
+  quantity NUMERIC(18,4) NOT NULL DEFAULT '0.0000',
+  unit_price NUMERIC(19,4) DEFAULT '0.0000',
+  discount DOUBLE PRECISION NOT NULL DEFAULT '0',
+  status_id INT,
+  date_allocated TIMESTAMP,
+  purchase_order_id INT,
+  inventory_id INT,
+  CONSTRAINT fk_order_details_orders1
+    FOREIGN KEY (order_id)
+    REFERENCES northwind.orders (id)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT fk_order_details_products1
+    FOREIGN KEY (product_id)
+    REFERENCES northwind.products (id)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT fk_order_details_order_details_status1
+    FOREIGN KEY (status_id)
+    REFERENCES northwind.order_details_status (id)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION
+);
+
+-- Create indexes
+CREATE INDEX IF NOT EXISTS idx_order_details_id ON northwind.order_details (id);
+CREATE INDEX IF NOT EXISTS idx_order_details_inventory_id ON northwind.order_details (inventory_id);
+CREATE INDEX IF NOT EXISTS idx_order_details_product_id ON northwind.order_details (product_id);
+CREATE INDEX IF NOT EXISTS idx_order_details_purchase_order_id ON northwind.order_details (purchase_order_id);
+CREATE INDEX IF NOT EXISTS idx_order_details_order_id ON northwind.order_details (order_id);
+CREATE INDEX IF NOT EXISTS idx_order_details_status_id ON northwind.order_details (status_id);
+
+
+CREATE TABLE IF NOT EXISTS northwind.purchase_order_details (
+  id SERIAL PRIMARY KEY,
+  purchase_order_id INT NOT NULL,
+  product_id INT,
+  quantity NUMERIC(18,4) NOT NULL,
+  unit_cost NUMERIC(19,4) NOT NULL,
+  date_received TIMESTAMP,
+  posted_to_inventory BOOLEAN NOT NULL DEFAULT false,
+  inventory_id INT,
+  CONSTRAINT fk_purchase_order_details_inventory_transactions1
+    FOREIGN KEY (inventory_id)
+    REFERENCES northwind.inventory_transactions (id)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT fk_purchase_order_details_products1
+    FOREIGN KEY (product_id)
+    REFERENCES northwind.products (id)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT fk_purchase_order_details_purchase_orders1
+    FOREIGN KEY (purchase_order_id)
+    REFERENCES northwind.purchase_orders (id)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION
+);
+
+-- Create indexes
+CREATE INDEX IF NOT EXISTS idx_purchase_order_details_id ON northwind.purchase_order_details (id);
+CREATE INDEX IF NOT EXISTS idx_purchase_order_details_inventory_id ON northwind.purchase_order_details (inventory_id);
+CREATE INDEX IF NOT EXISTS idx_purchase_order_details_product_id ON northwind.purchase_order_details (product_id);
+CREATE INDEX IF NOT EXISTS idx_purchase_order_details_purchase_order_id ON northwind.purchase_order_details (purchase_order_id);
+
+
+
+CREATE TABLE IF NOT EXISTS northwind.sales_reports (
+  "group_by" VARCHAR(50) NOT NULL,
+  display VARCHAR(50),
+  title VARCHAR(50),
+  filter_row_source TEXT,
+  "default" BOOLEAN NOT NULL DEFAULT false,
+  PRIMARY KEY ("group_by")
+);
+
+CREATE TABLE IF NOT EXISTS northwind.strings (
+  string_id SERIAL PRIMARY KEY,
+  string_data VARCHAR(255)
+);
 
